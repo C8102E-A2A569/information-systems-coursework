92eddcb1fa9dce37623d8a895c46402f856a0948
E-v-e-l-i-n-a <evelinasultan@mail.ru>
2025-01-31 15:07:31 +0300

    feat: configure Spring Security and JWT authentication

---
+++ backend/src/main/java/com/coursework/backend/entity/User.java
@@ -0,0 +1,63 @@
+package com.coursework.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public String getRole() {
+//        TODO: Изменить, когда появятся роли
+        return "ROLE_ADMIN";
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        TODO: Изменить, когда появятся роли
+        return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
