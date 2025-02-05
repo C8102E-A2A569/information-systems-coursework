93cbe729ec087e6d39942ee0303216b6e6163c85
E-v-e-l-i-n-a <evelinasultan@mail.ru>
2025-02-05 15:19:35 +0300

    fix: Исправила инициализацию папок: убрала присваивание пользователю группы ролей

--- backend/src/main/java/com/coursework/backend/user/model/User.java
+++ backend/src/main/java/com/coursework/backend/user/model/User.java
@@ -44,10 +44,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return userGroupRoles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getRole().getAuthority()))
-                .collect(Collectors.toSet());
+        return Collections.emptyList();
     }
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
